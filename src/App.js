06a9bc8ef7fcd8689bef904c6426df83a0831648
MinhHien39 <tranminhhien06072001@gmail.com>
2022-12-09 18:01:05 +0700

    C1 - React Hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from 'react'
+
+const orders = [100, 200 , 300]
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  // const [info , setInfo] =useState({
+  //   name : 'Trần Minh Hiển',
+  //   age : 21,
+  //   address :'Đà Nẵng , Việt Nam'
+  // })
+
+  // const handleUpdate = () => {
+  //   setInfo({
+  //     ...info,
+  //     bio :'Màu đỏ'
+  //   })
+  // }
+
+  // const total = orders.reduce((total , cur)=> total + cur);
+
+  // const [counter , setCouter] = useState(()=>{
+  //   const total = orders.reduce((total , cur)=> total + cur)
+  //   console.log(total);
+  //   return total;
+  // })
+
+  // const handleIncrease = () =>  {
+  //   setCouter(prevState => prevState +1 )
+  //   setCouter(prevState => prevState +1 )
+  //   setCouter(prevState => prevState +1 )
+
+  // }
+  const giftList =['CPU i9 ' ,'Ram 32 GB RGB' ,'RGB KeyBoard']
+
+  const [gift , setGift]= useState()
+
+  const randomGift = () => {
+    const index = Math.floor(Math.random() * giftList.length)
+    console.log(index);
+
+    setGift(giftList[index])
+    }
+  return (
+    <div className="App" style={{padding :20}}>
+      <h1>{gift || 'Chưa có phần thưởng'}</h1>
+      <button onClick={randomGift}>Lấy thưởng</button>
+    </div>
+  )
+}
 export default App;
